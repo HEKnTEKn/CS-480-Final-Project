9f293133f396c5c3770fba9de1e29f2f894cc53f
Victor Fong <vfong3@uic.edu>
2021-05-02 23:07:06 -0500

    fixes to GUI

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import PySimpleGUI as sg
 import workingDatabase
 sqlDatabase = workingDatabase.DB()
 
-sqlDatabase.performTextQuery("SELECT * FROM matches2020;")
 
-#sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
 
-sql_searh = [
+sql_search = [
     [
-        sg.Text("Search Esport"),
+        sg.Text("Search E-sport"),
         sg.In(size=(25, 1), enable_events=True, key="-esport-"),
 
     ],
     [
         sg.Listbox(
-            values=["Games Won", "Games Lost", "Player With Most Points", "Game Stats"], enable_events=True, size=(75, 36), key="-FILE LIST-"
+            values=["Games Won",
+                    "Games Lost",
+                    "Player With Most Points",
+                    "Game Stats"], enable_events=True, size=(75, 36), key="-FILE LIST-"
         )
     ],
 ]
 
 sql_output = [
-
     [
         sg.Listbox(
             values=[], enable_events=True, size=(75, 36), key="-OUT LIST-"
@@ -30,7 +30,7 @@ sql_output = [
 ]
 layout = [
     [
-        sg.Column(sql_searh),
+        sg.Column(sql_search),
         sg.VSeperator(),
         sg.Column(sql_output),
     ]
@@ -38,9 +38,16 @@ layout = [
 
 window = sg.Window("Image Viewer", layout)
 
-# Run the Event Loop
-event, values = window.read()
-if event == 'submit':
-    sqlDatabase.performInternalQuery(values[0], values[1])
+while True:
+    # Run the Event Loop
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+
+    if event == 'submit':
+        sqlDatabase.performInternalQuery(values[0], values[1])
+
+
 
 window.close()
